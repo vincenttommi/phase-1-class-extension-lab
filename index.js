3931c344e944ba1ee7fd91d23ae3b084c7cb2205
vincenttommi <vincenttommi@gmail.com>
2023-04-12 13:36:16 +0300

    extending lab

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Your code here
+class Polygon {
+    constructor(sides) {
+      this.sides = sides;
+    }
+  
+    get countSides() {
+      return this.sides.length;
+    }
+  
+    get perimeter() {
+      return this.sides.reduce((acc, side) => acc + side, 0);
+    }
+  }
+  
+  class Triangle extends Polygon {
+    get isValid() {
+      const [a, b, c] = this.sides;
+      return a + b > c && b + c > a && c + a > b;
+    }
+
+
+    
+
+
+
+
+    
+  }
+  
